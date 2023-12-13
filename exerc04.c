4845b8a4a1000f5954a5ce519c157eb0a72e81ef
Albert Claudio <117605854+albert-claudio@users.noreply.github.com>
2023-12-13 20:53:24 -0300

    exerc04.c

--- exerc04.c
+++ exerc04.c
@@ -3,26 +3,77 @@
 #include <string.h>
 #include <locale.h>
 
-struct lista_funcionarios
+struct Funcionario
 {
     float salario;
     char nome[30];
     char cargo[30];
 };
 
-void cabecalho(){
+void cabecalho()
+{
     printf("\n==============================================\n");
     printf("\t ODYSSEY INC.\n");
     printf("\n==============================================\n");
 }
 
-void CargosDeFuncionario(struct lista_funcionarios *funcionario, char *nome){
-    int i;
+float calcularMediaSalarial(struct Funcionario *funcionarios, char *cargo, int numFuncionarios)
+{
+    float somaSalarios = 0;
+    int contador = 0;
 
-    for ( i = 0; i < 5; i++)
+    for (int i = 0; i < numFuncionarios; i++)
     {
-        /* code 
+        if (strcmp(funcionarios[i].cargo, cargo) == 0)
+        {
+            somaSalarios += funcionarios[i].salario;
+            contador++;
+        }
     }
-    
 
+    if (contador > 0)
+    {
+        return somaSalarios / contador;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+  setlocale(LC_ALL, "portuguese");
+    struct Funcionario funcionarios[5];
+
+    
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Digite o nome do funcionário %d: ", i + 1);
+        scanf("%s", funcionarios[i].nome);
+
+        printf("Digite o cargo do funcionário %d: ", i + 1);
+        scanf("%s", funcionarios[i].cargo);
+
+        printf("Digite o salário do funcionário %d: ", i + 1);
+        scanf("%f", &funcionarios[i].salario);
+    }
+
+  
+    cabecalho();
+
+  
+    char cargoProcurado[] = "programador";
+    float mediaSalarial = calcularMediaSalarial(funcionarios, cargoProcurado, 5);
+
+    if (mediaSalarial > 0)
+    {
+        printf("Média Salarial dos Programadores: %.2f\n", mediaSalarial);
+    }
+    else
+    {
+        printf("Não há programadores na empresa.\n");
+    }
+
+    return 0;
 }
