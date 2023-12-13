0dd870fdc5262490b5188b1c88d51b4db81bfff0
Albert Claudio <117605854+albert-claudio@users.noreply.github.com>
2023-12-13 20:37:31 -0300

    exerc02.c

--- exerc02.c
+++ exerc02.c
@@ -3,41 +3,61 @@
 #include <stdlib.h>
 #include <locale.h>
 
-
-struct dados_aluno
+struct Aluno
 {
     char nome[200];
-    float notas[3];
+    char data_nascimento[20];
+    float notas[2];
     float media;
 };
 
-void calcularMedia(struct dados_aluno *alunos){
-    int i,j;
-    float somaNotas = 0;
-
-    for ( i = 0; i < TAM; i++)
-    {
-        for (j = 0; j < notas; j++)
-        {
-            somaNotas += alunos[i].notas[j];
-        }
-        alunos[i].media = somaNotas / NOTAS;
-        somaNotas = 0;
+float calcularMedia(struct Aluno aluno)
+{
+    return (aluno.notas[0] + aluno.notas[1]) / 2.0;
 }
- //CALCULANDO A NOTA
- alunos[i] = calcularMedia(alunos);
 
- //MOSTRANDO A NOTA
- for (i = 0; i < TAM; i++)
- {
-    printf("\nNome: %s \n", aluno[i].nome);
-    printf("\nSoma: %s \n", aluno[i].soma);
-    printf("\nMedia: %s \n", aluno[i].media);
- }
- }
+void verificarAprovacao(struct Aluno aluno)
+{
+    if (aluno.media >= 7.0)
+    {
+        printf("%s está aprovado!\n", aluno.nome);
+    }
+    else
+    {
+        printf("%s está reprovado.\n", aluno.nome);
+    }
+}
 
- int main(){
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+    struct Aluno alunos[5];
 
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Digite o nome do aluno %d: ", i + 1);
+        scanf("%s", alunos[i].nome);
+
+        printf("Digite a data de nascimento do aluno %d: ", i + 1);
+        scanf("%s", alunos[i].data_nascimento);
+
+        printf("Digite a nota 1 do aluno %d: ", i + 1);
+        scanf("%f", &alunos[i].notas[0]);
+
+        printf("Digite a nota 2 do aluno %d: ", i + 1);
+        scanf("%f", &alunos[i].notas[1]);
+
+        
+        alunos[i].media = calcularMedia(alunos[i]);
+    }
+
+    
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\nAluno: %s\n", alunos[i].nome);
+        printf("Média: %.2f\n", alunos[i].media);
+        verificarAprovacao(alunos[i]);
+    }
 
     return 0;
- }
+}
